91038ee61c63952308f880e3eb74966734199109
CanHaskan <canhaskan1903@hotmail.com>
2025-11-22 00:27:30 +0300

    Creating current run page, slider and map authorizations

---
+++ RunApp/Controller/CurrentRunVC.swift
@@ -0,0 +1,94 @@
+//
+//  CurrentRunVC.swift
+//  RunApp
+//
+//  Created by Can Haskan on 21.11.2025.
+//
+
+import UIKit
+import MapKit
+
+class CurrentRunVC: LocationVC {
+    
+    @IBOutlet weak var swipeBGImg: UIImageView!
+    @IBOutlet weak var sliderImg: UIImageView!
+    @IBOutlet weak var duationLbl: UILabel!
+    @IBOutlet weak var paceLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var pauseBtn: UIButton!
+    
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    
+    var runDistance = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
+        sliderImg.addGestureRecognizer(swipeGesture)
+        sliderImg.isUserInteractionEnabled = true
+        swipeGesture.delegate = self as? UIGestureRecognizerDelegate
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.distanceFilter = 10
+        startRun()
+    }
+    
+    func startRun() {
+        manager?.startUpdatingLocation()
+    }
+    
+    func endRun() {
+        manager?.stopUpdatingLocation()
+    }
+    
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+    }
+    
+    @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
+        let minAdjust: CGFloat = 80
+        let maxAdjust: CGFloat = 128
+        if let sliderView = sender.view {
+            if sender.state == UIGestureRecognizer.State.began || sender.state == UIGestureRecognizer.State.changed {
+                let translation = sender.translation(in: self.view)
+                if sliderView.center.x >= (swipeBGImg.center.x - minAdjust) && sliderView.center.x <= (swipeBGImg.center.x + maxAdjust) {
+                    sliderView.center.x = sliderView.center.x + translation.x
+                } else if sliderView.center.x >= (swipeBGImg.center.x + maxAdjust) {
+                    sliderView.center.x = swipeBGImg.center.x + maxAdjust
+                    
+                    dismiss(animated: true, completion: nil)
+                } else {
+                    sliderView.center.x = swipeBGImg.center.x - minAdjust
+                }
+                
+                sender.setTranslation(CGPoint.zero, in: self.view)
+            } else if sender.state == UIGestureRecognizer.State.ended {
+                UIView.animate(withDuration: 0.1, animations: {
+                    sliderView.center.x = self.swipeBGImg.center.x - minAdjust
+                })
+            }
+        }
+        
+    }
+}
+
+extension CurrentRunVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            if status == .authorizedWhenInUse {
+                checkLocationAuthStatus()
+            }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if startLocation == nil {
+            startLocation = locations.first
+        } else if let location = locations.last {
+            runDistance += lastLocation.distance(from: location)
+            distanceLbl.text = "\(runDistance)"
+        }
+        lastLocation = locations.last
+    }
+}
---
+++ RunApp/Controller/LocationVC.swift
@@ -0,0 +1,32 @@
+//
+//  LocationVC.swift
+//  RunApp
+//
+//  Created by Can Haskan on 21.11.2025.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, MKMapViewDelegate {
+    
+    var manager: CLLocationManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+    }
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager?.requestWhenInUseAuthorization()
+        }
+    }
+    
+
+    
+    
+
+}
