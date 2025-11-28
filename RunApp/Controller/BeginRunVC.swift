4a29c835788ff6b94b73bea5918a144750200da2
CanHaskan <canhaskan1903@hotmail.com>
2025-11-28 22:34:53 +0300

    Added last run view to the beginRun VC.

--- RunApp/Controller/BeginRunVC.swift
+++ RunApp/Controller/BeginRunVC.swift
@@ -11,6 +11,12 @@ import MapKit
 class BeginRunVC: LocationVC {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var lastRunCloseBtn: UIButton!
+    @IBOutlet weak var speedLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var durationLbl: UILabel!
+    @IBOutlet weak var lastRunBGView: UIView!
+    @IBOutlet weak var lastRunStack: UIStackView!
     
     
     override func viewDidLoad() {
@@ -22,14 +28,38 @@ class BeginRunVC: LocationVC {
     override func viewWillAppear(_ animated: Bool) {
         manager?.delegate = self
         manager?.startUpdatingLocation()
+        getLastRun()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         manager?.stopUpdatingLocation()
     }
     
+    func getLastRun() {
+        guard let lastRun = Run.getAllRuns()?.first else {
+            lastRunStack.isHidden = true
+            lastRunBGView.isHidden = true
+            lastRunCloseBtn.isHidden = true
+            return
+        }
+        lastRunStack.isHidden = false
+        lastRunBGView.isHidden = false
+        lastRunCloseBtn.isHidden = false
+        speedLbl.text = "Speed: \(lastRun.speed) km/h"
+        distanceLbl.text = "Distance: \(lastRun.distance.roundedNumber(to: 2)) km"
+        durationLbl.text = "Duration: \(lastRun.duration.formatTimeDurationToString())"
+        
+        
+        
+    }
     
-
+    
+    @IBAction func lastRunCloseBtnPressed(_ sender: Any) {
+        lastRunStack.isHidden = true
+        lastRunBGView.isHidden = true
+        lastRunCloseBtn.isHidden = true
+    }
+    
     @IBAction func locationCenterBtnPressed(_ sender: Any) {
     }
     
